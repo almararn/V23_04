22494a03c7a69fa05ae39635dfb01220580a105f
almararn <almararn@gmail.com>
2023-03-14 15:28:29 +0000

    finished

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./deadpool-tailwind.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
